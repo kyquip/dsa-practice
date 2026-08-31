26f9c905be7b44a7aca709b5d7f4519863dd837e
Rudra Kumar Mishra <kyquip@icloud.com>
2026-08-31 20:51:05 +0530

    Solved Find First Position Greater Equal Target

---
+++ binary_search/06_find_first_position_greater_equal_target.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+    int findFirstPositionGreaterEqualTarget(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int mid = left + (right - left) / 2;
+        while(left <= right) {
+            if(nums[mid] == target) {
+                while (nums[mid] == target) mid--;
+                return mid + 1;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else { left = mid + 1; }
+
+            mid = left + (right - left) / 2;
+        }
+        return -1;
+    }
+
+    public: Solution() {
+        vector<int> nums = {1, 2, 4, 4, 4, 6, 8};
+        cout << "First Position is " << findFirstPositionGreaterEqualTarget(nums, 4) << endl;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution s;
+    return 0;
+}
