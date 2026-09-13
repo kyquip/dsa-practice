966201ac51aee60af4ac7a3a98ba8291b71ad4eb
Rudra Kumar Mishra <kyquip@icloud.com>
2026-09-13 22:13:06 +0530

    Update Trapping Rain Water
    Previously used Two Pointer but asked to use PrefixSum

--- array/29_trapping_rain_water.cpp
+++ array/29_trapping_rain_water.cpp
@@ -5,27 +5,26 @@ using namespace std;
 
 class Solution{
     int trappingRainWater(vector<int> &height) {
-        int left = 0, right = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-        
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    water += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    water += rightMax - height[right];
-                }
-                right--;
-            }
+        int n = height.length;
+        if (n <= 2) return 0;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
+
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int water = 0;
+        for (int i = 0; i < n; i++) {
+            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
         return water;
     }
 
