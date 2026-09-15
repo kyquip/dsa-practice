37013913660e71ff0d676b49a65df3a899b1c443
Rudra Kumar Mishra <kyquip@icloud.com>
2026-09-15 17:13:50 +0530

    Solved Find Rotation Count

---
+++ binary_search/19_find_rotation_count.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+    int findRotationCount(vector<int> &nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right])  left = mid + 1;
+            else  right = mid;
+        }
+
+        return left;
+    }
+
+    public: Solution() {
+        vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+        cout << "Rotation Count is " << findRotationCount(nums) << endl;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution s;
+    return 0;
+}
